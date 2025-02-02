5d4e1fdb81acd7108428c654154dfa5c7d3b3e6c
cowilliams_deloitte <cowilliams@deloitte.com>
2025-02-02 16:17:54 -0700

    added project from section 3 for lesson 17.

---
+++ angular-testing-course-testing-pagination-component/src/app/shared/components/pagination/pagination.component.spec.ts
@@ -0,0 +1,5 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PaginationComponent } from './pagination.component';
+import { UtilsService } from '../../services/utils.service';
+import { By } from '@angular/platform-browser';
+import { first } from 'rxjs';
